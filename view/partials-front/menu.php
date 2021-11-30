36629aafac683852be30e6cd8b427d9f22a2cd77
codeorafk <63441848+codeorafk@users.noreply.github.com>
2021-12-01 01:35:23 +0700

    new init

--- view/partials-front/menu.php
+++ view/partials-front/menu.php
@@ -18,7 +18,7 @@
 
 <body>
     <!-- Navbar Section Starts Here -->
-    <section class="navbar" style="position:fixed; background-color: #f7f1e3; width: 100%;height: 50px; z-index: 2;">
+    <section class="navbar" style="padding:0;padding-left: 10px; position:fixed; background-color: #f7f1e3; width: 100%;height: 50px; z-index: 2;">
             <div>
                 <a href="<?php echo view; ?>" title="Logo" >
                     <img src="<?php echo Ppath; ?>images/logo.png" alt="Restaurant Logo" class="img-responsive" style="height: 40px;">
@@ -26,7 +26,7 @@
             </div>
 
             <div>
-                <ul>
+                <ul style="margin: auto">
                 <li>
                         <button id="cart" type="button" class="" data-toggle="modal" data-target="#cartModal" style="background-color: #f7f1e3; color: black; border: none"> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
